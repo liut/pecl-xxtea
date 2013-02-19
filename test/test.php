17776e82caaf43a09b80fd106c63f107735f0e65
Liut <Eagle.L@gmail.com>
2013-02-19 15:51:52 +0800

    fix test

--- test/test.php
+++ test/test.php
@@ -1,6 +1,6 @@
 <?php
-//echo "empty:empty\t", xxtea_decrypt(xxtea_encrypt("", ""), ""), "\n";
-//echo "1:empty\t", xxtea_decrypt(xxtea_encrypt("1", ""), ""), "\n";
+
+echo "empty:1\t", xxtea_decrypt(xxtea_encrypt("", "1"), "1"), "\n";
 echo "1:1\t", xxtea_decrypt(xxtea_encrypt("1", "1"), "1"), "\n";
 echo "str:key\t", xxtea_decrypt(xxtea_encrypt("12222222222222", "2222222222222222"), "2222222222222222"), "\n";
 echo "str:key\t", xxtea_decrypt(xxtea_encrypt("12222222222222", "22222222222"), "22222222222"), "\n";
@@ -9,6 +9,7 @@ print_r(xxtea_info());
 
 echo 'A:A', "\t"; // string(12) "bKIlXWXSJvE="
 var_dump(base64_encode(xxtea_encrypt('A','A')));
+echo 'A:AAAAAAAAAAAAAAAA', "\t"; // string(12) "Q714fFUVK6k="
+var_dump(base64_encode(xxtea_encrypt('A','AAAAAAAAAAAAAAAA')));
 echo PHP_EOL;
-
 
